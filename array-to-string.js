0c9b957491241d2947808aa77d617f49e0702467
John Wasielewicz <wazjohn@yahoo.com>
2021-07-17 13:35:15 -0500

    add arrayToString to js practice

---
+++ array-to-string.js
@@ -0,0 +1,6 @@
+function arrayToString(arr) {
+    let str = arr.join(" ");
+    return str;
+  }
+  
+  arrayToString(['Big', 'Trouble', 'In', 'Little', 'China']); // Expected Output: Big Trouble In Little China
