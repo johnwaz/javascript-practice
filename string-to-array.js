ccca914db9dc62ae6d26e151088afca3fb222266
John Wasielewicz <wazjohn@yahoo.com>
2021-07-17 21:56:06 -0500

    add stringToArray to js practice

---
+++ string-to-array.js
@@ -0,0 +1,6 @@
+function stringToArray(str) {
+    let arr = str.split(" ");
+    return arr;
+  }
+  
+  stringToArray("Big Trouble In Little China"); // Expected Output: [ 'Big', 'Trouble', 'In', 'Little', 'China' ]
